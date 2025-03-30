9bd356a6fb6f18b8466e1751420865942780e95b
André Alves <andre.alves2006@hotmail.com>
2025-03-30 19:31:05 -0300

    Initializing the database

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,20 @@ from flask import Flask, render_template
 def create_app():
   app = Flask(__name__, instance_relative_config=True)
 
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'app.sqlite')
+  )
+
   try:
     os.makedirs(app.instance_path)
   except OSError:
     pass
+  
+  from . import db
+  db.init_app(app)
 
-  @app.route('/')
-  def index():
-    return render_template('index.html')
+  from . import auth
+  app.register_blueprint(auth.bp)
 
   return app
